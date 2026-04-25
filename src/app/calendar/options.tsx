993911b9cfdc8613d2ea3ddc442fa40c5f066ef7
Evan-Peuvergne <evanpeuvergne@gmail.com>
2026-04-25 12:29:05 +0200

    Implement dropdown in calendar options

--- src/app/calendar/options.tsx
+++ src/app/calendar/options.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react"
 import * as Styles from "./styles"
 import { Secondary, SecondaryIcon } from "@components/button"
+import { Dropdown } from "@components/dropdown"
+import { Switch } from "@components/switch"
 import { formatWeekRange } from "./utils"
 
 export interface CalendarOptionsProps {
@@ -8,17 +11,38 @@ export interface CalendarOptionsProps {
 }
 
 export const CalendarOptions = ({ weekStart, navigate }: CalendarOptionsProps) => {
+  const [weekends, setWeekends] = useState(true)
+  const [declinedEvents, setDeclinedEvents] = useState(true)
+  const [completedTasks, setCompletedTasks] = useState(false)
+
   return (
     <div className={Styles.options}>
       <Secondary onClick={() => navigate(new Date())}>Go to today</Secondary>
       <hr />
       <span style={{ display: "inline-flex", gap: 0 }}>
-        <SecondaryIcon icon="chevron-left"  onClick={() => navigate(new Date(weekStart.getTime() - 7 * 86_400_000))} />
+        <SecondaryIcon icon="chevron-left" onClick={() => navigate(new Date(weekStart.getTime() - 7 * 86_400_000))} />
         <Secondary>{formatWeekRange(weekStart)}</Secondary>
         <SecondaryIcon icon="chevron-right" onClick={() => navigate(new Date(weekStart.getTime() + 7 * 86_400_000))} />
       </span>
       <hr />
-      <SecondaryIcon icon="more" />
+      <Dropdown button={<SecondaryIcon icon="more" />}>
+        {(ref, style, close) => (
+          <Dropdown.Overlay ref={ref as React.Ref<HTMLDivElement>} style={style}>
+            <Switch as={Dropdown.Option} checked={weekends} onChange={setWeekends}>
+              View week-ends
+            </Switch>
+            <Switch as={Dropdown.Option} checked={declinedEvents} onChange={setDeclinedEvents}>
+              View declined events
+            </Switch>
+            <Switch as={Dropdown.Option} checked={completedTasks} onChange={setCompletedTasks}>
+              View completed tasks
+            </Switch>
+            <Dropdown.Divider />
+            <Dropdown.Option icon="calendar" onClick={close}>Lorem ipsum</Dropdown.Option>
+            <Dropdown.Option icon="calendar" onClick={close}>Lorem ipsum</Dropdown.Option>
+          </Dropdown.Overlay>
+        )}
+      </Dropdown>
     </div>
   )
 }
